cdba7eded421c00ad6a3bb8ef43c59a8523151ec
Michael Noon <m.noon83@yahoo.com>
2021-10-13 18:13:39 -0700

    code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,2 +1,19 @@
+const { isBuffer } = require('lodash')
+const { User } = require('../models')
+const jwt=require('jsonwebtoken')
+const { resolveAny } = require('dns/promises')
 const router=require('express').Router()
+router.post('/users/register',(req,res)=>{
+  User.register(new User({username: req.body.userman}),req.body.password,err=>{
+    if(err){onsole.log(err)}
+    res.sendStatus(200)
+  })
+})
+router.post('/users/login',(req,req)=>{
+  User.authenticate()(req.body.password,(err,user)=>{
+    if(err){console.log(err)}
+    res.json(user ? jwt.sign({id:user.id}, process.env.SECRET))
+  })
+})
+
 module.exports=router
